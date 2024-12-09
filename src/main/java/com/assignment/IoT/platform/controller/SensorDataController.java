4c16b6159615f480d285705128f74307831c5dd3
gangadhar <gangadhar.g@thoughtworks.com>
2024-12-09 18:57:23 +0530

    Add. validations for SensorData and CreateSensorDataRequest

--- src/main/java/com/assignment/IoT/platform/controller/SensorDataController.java
+++ src/main/java/com/assignment/IoT/platform/controller/SensorDataController.java
@@ -1,9 +1,9 @@
 package com.assignment.IoT.platform.controller;
 
 import com.assignment.IoT.platform.dto.request.CreateSensorDataRequest;
-import com.assignment.IoT.platform.dto.response.CreateSensorDataResponse;
 import com.assignment.IoT.platform.model.SensorData;
 import com.assignment.IoT.platform.service.SensorDataService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +19,7 @@ public class SensorDataController {
     }
 
     @PostMapping("/message")
-    public ResponseEntity<SensorData> createAndProduceSensorData(@RequestBody CreateSensorDataRequest createSensorDataRequest) {
+    public ResponseEntity<SensorData> createAndProduceSensorData(@Valid @RequestBody CreateSensorDataRequest createSensorDataRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(sensorDataService.createAndProduceSensorData(createSensorDataRequest));
     }
 }
--- src/main/java/com/assignment/IoT/platform/dto/request/CreateSensorDataRequest.java
+++ src/main/java/com/assignment/IoT/platform/dto/request/CreateSensorDataRequest.java
@@ -17,22 +17,22 @@ public class CreateSensorDataRequest {
     @NotBlank(message = "Sensor Id is must for sensorData")
     private String sensorId;
 
-    @NotBlank(message = "Temperature is required")
+    @NotNull(message = "Temperature is required")
     @Min(value = -90, message = "Temperature cannot be less than -90°C")
     @Max(value = 70, message = "Temperature cannot exceed 70°C")
     private Double temperature;
 
-    @NotBlank(message = "Latitude is required")
+    @NotNull(message = "Latitude is required")
     @DecimalMin(value = "-90.0", inclusive = true, message = "Latitude must be between -90 and 90")
     @DecimalMax(value = "90.0", inclusive = true, message = "Latitude must be between -90 and 90")
     private Double latitude;
 
-    @NotBlank(message = "Longitude is required")
+    @NotNull(message = "Longitude is required")
     @DecimalMin(value = "-180.0", inclusive = true, message = "Longitude must be between -180 and 180")
     @DecimalMax(value = "180.0", inclusive = true, message = "Longitude must be between -180 and 180")
     private Double longitude;
 
-    @NotBlank(message = "Battery percentage is required")
+    @NotNull(message = "Battery percentage is required")
     @DecimalMin(value = "0", inclusive = true, message = "Battery percentage cannot be lesser than 0")
     @DecimalMax(value = "100", inclusive = true, message = "Battery percentage cannot be greater than 100")
     private Double batteryPercentage;
