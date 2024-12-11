36b9f8fd13af4cfab0c9d9baf73399fcdc58413e
gangadhar <gangadhar.g@thoughtworks.com>
2024-12-11 21:06:56 +0530

    Add. Kafka consumer configuration

--- src/main/java/com/assignment/IoT/platform/configurations/KafkaConfig.java
+++ src/main/java/com/assignment/IoT/platform/configurations/KafkaConfig.java
@@ -1,13 +1,15 @@
 package com.assignment.IoT.platform.configurations;
 
-import com.assignment.IoT.platform.dto.request.CreateSensorDataRequest;
 import com.assignment.IoT.platform.model.SensorData;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,15 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.producer.value-serializer}")
     private String producerValueSerializer;
+
+    @Value("${spring.kafka.consumer.key-deserializer}")
+    private String consumerKeyDeserializer;
+
+    @Value("${spring.kafka.consumer.value-deserializer}")
+    private String consumerValueDeserializer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
 
     @Bean
     public Map<String, Object> producerConfigs() {
@@ -43,4 +54,20 @@ public class KafkaConfig {
     public KafkaTemplate<String, SensorData> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, consumerKeyDeserializer);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, consumerValueDeserializer);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, SensorData> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
 }
